67298a32ba44f6ad9f37365b4324bb8bbbd7e343
Camilo Lozada <Cristian.lozada@unillanos.edu.co>
2023-05-20 21:42:08 -0500

    first commit

---
+++ 2-globals.js
@@ -0,0 +1,12 @@
+// GLOBALS  - NO WINDOW !!!!
+
+// __dirname   - path to current directory
+// __filename  - file name
+// require     - function to use modules (CommonJS)
+// module      - info about current module (file)
+// process     - info about env where the progream is being executed
+
+console.log('This is the directory name-> '+__dirname);
+setInterval(()=>{
+    console.log('Hello World')
+},1000);
